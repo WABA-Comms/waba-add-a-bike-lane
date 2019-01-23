7a3b9ff10337884bc073208e834713b7641e1ad3
Nicki Dlugash <nicki@mapbox.com>
2019-01-23 16:19:10 -0500

    add income data, add display for secondary text sections

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ map.on('load', function() {
   var corridorInfo = [
     {
       id: 'crashes-total',
-      text: 'total crashes',
+      text: 'Total crashes',
       geomType: 'point',
       data: turf.featureCollection(map.querySourceFeatures('composite', {
         sourceLayer: 'Crashes_in_DC-d0weq7'
@@ -34,7 +34,7 @@ map.on('load', function() {
     },
     {
       id: 'crashes-cyclists',
-      text: 'bike-related crashes',
+      text: 'Bike-related crashes',
       geomType: 'point',
       data: turf.featureCollection(map.querySourceFeatures('composite', {
         sourceLayer: 'Crashes_in_DC-d0weq7',
@@ -43,7 +43,7 @@ map.on('load', function() {
     },
     {
       id: 'crashes-pedestrians',
-      text: 'pedestrian-related crashes',
+      text: 'Pedestrian-related crashes',
       geomType: 'point',
       data: turf.featureCollection(map.querySourceFeatures('composite', {
         sourceLayer: 'Crashes_in_DC-d0weq7',
@@ -52,27 +52,78 @@ map.on('load', function() {
     },
     {
       id: 'population',
-      text: 'population',
+      text: 'Population',
       geomType: 'polygon',
       data: turf.featureCollection(map.querySourceFeatures('composite', {
         sourceLayer: 'combined_features-7kmirr'
       })),
-      measurements: ['population_total']
+      dataFields: ['population_total']
     },
     {
       id: 'modeshare',
-      text: 'modeshare',
+      text: 'Modeshare',
+      secondaryText: [
+        'Bicycle',
+        'Automotive',
+        'Walking',
+        'Public transportation'
+      ],
       geomType: 'polygon',
       data: turf.featureCollection(map.querySourceFeatures('composite', {
         sourceLayer: 'combined_features-7kmirr',
       })),
-      measurements: [
+      dataFields: [
         'transport_bicycle',
         'transport_car_truck_or_van',
         'transport_walked',
         'transport_public_transportation_excluding_taxicab'
         ],
       total: 'transport_total'
+    },
+    {
+      id: 'income',
+      text: 'Income',
+      secondaryText: [
+        '<10,000',
+        '10,000–14,999',
+        '15,000–19,999',
+        '20,000–24,999',
+        '25,000–29,999',
+        '30,000–34,999',
+        '35,000–39,999',
+        '40,000–44,999',
+        '45,000–49,999',
+        '50,000–59,999',
+        '60,000–74,999',
+        '5,000–99,999',
+        '100,000–124,999',
+        '125,000–149,999',
+        '150,000–199,999',
+        '>200,000'
+      ],
+      geomType: 'polygon',
+      data: turf.featureCollection(map.querySourceFeatures('composite', {
+        sourceLayer: 'combined_features-7kmirr',
+      })),
+      dataFields: [
+        'income_less_than_10_000',
+        'income_10_000_to_14_999',
+        'income_15_000_to_19_999',
+        'income_20_000_to_24_999',
+        'income_25_000_to_29_999',
+        'income_30_000_to_34_999',
+        'income_35_000_to_39_999',
+        'income_40_000_to_44_999',
+        'income_45_000_to_49_999',
+        'income_50_000_to_59_999',
+        'income_60_000_to_74_999',
+        'income_75_000_to_99_999',
+        'income_100_000_to_124_999',
+        'income_125_000_to_149_999',
+        'income_150_000_to_199_999',
+        'income_200_000_or_more'
+        ],
+      total: 'income_total'
     }
   ];
 
@@ -80,6 +131,7 @@ map.on('load', function() {
   //console.log(corridorInfo[3]);
 
   // Display census data on map to help debug
+  /*
   map.addLayer(
     {
       "id": "census-blocks",
@@ -108,6 +160,7 @@ map.on('load', function() {
       }
     }
   );
+  */
 
   map.addControl(draw);
 
@@ -124,6 +177,7 @@ map.on('load', function() {
     var bufferedCorridor = turf.buffer(corridor, 10, {units: 'meters'});
 
     // Display buffered corridor on map to help debug
+    /*
     map.addLayer(
       {
         "id": "bufferedCorridor",
@@ -134,10 +188,10 @@ map.on('load', function() {
         "type": "fill",
         "paint": {
           "fill-color": "#f00",
-          "fill-opacity": 0.5
+          "fill-opacity": 0.25
         }
       }
-    );
+    );*/
 
     for (item in corridorInfo) {
       // Type: Point
@@ -147,11 +201,14 @@ map.on('load', function() {
 
       // Type: Polygon
       if (corridorInfo[item].geomType === "polygon") {
-        var measurements = corridorInfo[item].measurements;
-        var totalCount = [];
-        measurements.forEach(function(){
-           totalCount.push(0);
-        });
+        var dataFields = corridorInfo[item].dataFields;
+        var count = [];
+        if (corridorInfo[item].total) {
+          var totalCount = 0;
+        }
+        dataFields.forEach(function() {
+          count.push(0);
+        })
         // Iterate through features to check for corridor overlap
         //console.log(corridorInfo[item]);
         for (i in corridorInfo[item].data.features) {
@@ -166,17 +223,35 @@ map.on('load', function() {
             // TODO: dedupe tiles using GEOID property (this is unique by block. Is there a better property?)
             // TODO: add optional % calculation
             // TODO: figure out why this function does seem to capture all intersecting census blocks
-            for (j in measurements) {
-            totalCount[j] += +feature.properties[measurements[j]];
+            for (j in dataFields) {
+            count[j] += +feature.properties[dataFields[j]];
             }
+            //if (corridorInfo[item].total) {
+            //totalCount += +feature.properties[total];
+            //var count = count / totalCount * 100;
+            //}
           }
-          var data = totalCount;
+          
+          var data = count;
         }
-      }
+      }  
 
       // Creates new rows in the table the first time a line is drawn
       // Replaces old data each time a new line is drawn
       // (There's probably better ways to update the table)
+
+      /*if (document.getElementById('corridor-info-table').rows.length > item) {
+        var row = document.getElementById(corridorInfo[item].id);
+        var desc = row.cells[0];
+        var more = row.cells[1];
+      } else {
+        var row = document.getElementById('corridor-info-table').insertRow(-1);
+        row.id = corridorInfo[item].id;
+        //console.log("Added row in corridor info table: " + row.id);
+        var desc = row.insertCell(0);
+        var more = row.insertCell(1);
+      };*/
+
       if (document.getElementById('corridor-info-table').rows.length > item) {
         var row = document.getElementById(corridorInfo[item].id);
         var desc = row.cells[0];
@@ -194,7 +269,13 @@ map.on('load', function() {
         desc.innerHTML =  corridorInfo[item].text + ': ' + '<span class="txt-bold">' + data[0] + '</span> ';
       } else {
         // TODO: Add additional rows for the sub-items
-        desc.innerHTML =  corridorInfo[item].text + ': ' + '<span class="txt-bold">' + "test" + '</span> ';
+        desc.innerHTML =  corridorInfo[item].text + '</br>';
+        for (j in corridorInfo[item].secondaryText) {
+          desc.innerHTML += '<p class=secondaryText>' + corridorInfo[item].secondaryText[j] + ': ' + '<span class="txt-bold">' + data[j] + '</span></p>';
+          //var secondaryText = document.createElement('p');
+          //secondaryText.innerHTML = '<p>' + corridorInfo[item].secondaryText[j] + ': ' + '<span class="txt-bold">' + data[j] + '</span></p>';
+          //desc.appendChild(secondaryText);
+        }
       };
       // "More" section is a placeholder for accessing:
       // 1. Information about the data the statistic was derived from
